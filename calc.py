fe9b945a6d9a821caa0e3d27771904193de5eb3e
cokorie <chijioke.okorie@gmail.com>
2023-06-27 14:57:48 -0500

    Add if-else statement, and addition condition

--- calc.py
+++ calc.py
@@ -1,3 +1,9 @@
 operator = input("Enter operator (+, -, * or /): ");
 num1 = float(input("Enter the first number: "));
 num2 = float(input("Enter the second number: "));
+
+if operator == "+":
+    answer = num1 + num2;
+    print(round(answer, 5));
+else:
+    print(f"{operator} is not valid.");
