b45eb976943ee4e1fa1586dce74b156a42668031
cokorie <chijioke.okorie@gmail.com>
2023-06-28 15:53:03 -0500

    retool for GUI version, and import Tkinter

--- calculator/main.py
+++ calculator/main.py
@@ -1,18 +1,12 @@
-operator = input("Enter operator (+, -, * or /): ");
-num1 = float(input("Enter the first number: "));
-num2 = float(input("Enter the second number: "));
+import tkinter as tkin;
 
-if operator == "+":
-    answer = num1 + num2;
-    print(round(answer, 5));
-elif operator == "-":
-    answer = num1 - num2;
-    print(round(answer, 5));
-elif operator == "*":
-    answer = num1 * num2;
-    print(round(answer, 5));
-elif operator == "/":
-    answer = num1 / num2;
-    print(round(answer, 5));
-else:
-    print(f"{operator} is not valid.");
+calculation = "";
+
+def add_to_calculation(symbol):
+    pass;
+
+def eval_calculation():
+    pass;
+
+def clear_field():
+    pass;
