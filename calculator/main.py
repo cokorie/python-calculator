12cb711bb6d97625f8da211522c942ae536ac7af
cokorie <chijioke.okorie@gmail.com>
2023-06-30 13:56:45 -0500

    add button 3 proper placement

--- calculator/main.py
+++ calculator/main.py
@@ -34,5 +34,6 @@ btn_1.grid(row=2, column=1);
 btn_2 = tk.Button(root, text="2", command=lambda: add_to_calculation(2), width=5, font=("Helvetica, 14"));
 btn_2.grid(row=2, column=2);
 btn_3 = tk.Button(root, text="3", command=lambda: add_to_calculation(3), width=5, font=("Helvetica, 14"));
+btn_3.grid(row=2, column=3);
 
 root.mainloop();
