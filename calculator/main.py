1caf835aa6bc65ffda6760822e3f85d5a761e612
cokorie <chijioke.okorie@gmail.com>
2023-06-30 14:12:20 -0500

    add button 9 to calculator

--- calculator/main.py
+++ calculator/main.py
@@ -45,5 +45,6 @@ btn_7 = tk.Button(root, text="7", command=lambda: add_to_calculation(7), width=5
 btn_7.grid(row=4, column=1);
 btn_8 = tk.Button(root, text="8", command=lambda: add_to_calculation(8), width=5, font=("Helvetica, 14"));
 btn_8.grid(row=4, column=2);
+btn_9 = tk.Button(root, text="9", command=lambda: add_to_calculation(9), width=5, font=("Helvetica, 14"));
 
 root.mainloop();
