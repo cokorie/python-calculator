884b9dd263b9357f67ecd24c0a6f0c6103fba9a6
cokorie <chijioke.okorie@gmail.com>
2023-07-01 11:16:31 -0500

    add propor gridding for open parentheses button

--- calculator/main.py
+++ calculator/main.py
@@ -72,5 +72,6 @@ btn_div = tk.Button(root, text="/", command=lambda: add_to_calculation("/"), wid
 btn_div.grid(row=5, column=4);
 
 btn_open_paren = tk.Button(root, text="(", command=lambda: add_to_calculation("("), width=5, font=("Helvetica, 14"));
+btn_open_paren.grid(row=5, column=1);
 
 root.mainloop();
