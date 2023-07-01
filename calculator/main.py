7e544eb58b93f149eded87faa9314c4f00ce51d1
cokorie <chijioke.okorie@gmail.com>
2023-07-01 11:09:14 -0500

    add proper gridding for multiplication button

--- calculator/main.py
+++ calculator/main.py
@@ -66,5 +66,6 @@ btn_minus = tk.Button(root, text="-", command=lambda: add_to_calculation("-"), w
 btn_minus.grid(row=3, column=4);
 
 btn_multi = tk.Button(root, text="*", command=lambda: add_to_calculation("*"), width=5, font=("Helvetica, 14"));
+btn_minus.grid(row=4, column=4);
 
 root.mainloop();
