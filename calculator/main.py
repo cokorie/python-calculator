133c9cc99e85ece81d09e2e302481ccd124e4500
cokorie <chijioke.okorie@gmail.com>
2023-07-07 15:07:25 -0500

    revert button 2 text to original size

--- calculator/main.py
+++ calculator/main.py
@@ -32,7 +32,7 @@ text_result.grid(columnspan=5);
 btn_1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1), width=5, font=("Arial, 14"));
 btn_1.grid(row=2, column=1);
 
-btn_2 = tk.Button(root, text="2", command=lambda: add_to_calculation(2), width=5, font=("Arial, 28"));
+btn_2 = tk.Button(root, text="2", command=lambda: add_to_calculation(2), width=5, font=("Arial, 14"));
 btn_2.grid(row=2, column=2);
 
 btn_3 = tk.Button(root, text="3", command=lambda: add_to_calculation(3), width=5, font=("Arial, 28"));
