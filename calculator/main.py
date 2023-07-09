8b44da7727d201fee309e4d2974de46b8e2ebd19
cokorie <chijioke.okorie@gmail.com>
2023-07-09 12:28:43 -0500

    change open parentheses button font to Courier

--- calculator/main.py
+++ calculator/main.py
@@ -71,7 +71,7 @@ btn_multi.grid(row=4, column=4);
 btn_div = tk.Button(root, text="/", command=lambda: add_to_calculation("/"), width=5, font=("Courier, 14"));
 btn_div.grid(row=5, column=4);
 
-btn_open_paren = tk.Button(root, text="(", command=lambda: add_to_calculation("("), width=5, font=("Arial, 14"));
+btn_open_paren = tk.Button(root, text="(", command=lambda: add_to_calculation("("), width=5, font=("Courier, 14"));
 btn_open_paren.grid(row=5, column=1);
 
 btn_close_paren = tk.Button(root, text=")", command=lambda: add_to_calculation(")"), width=5, font=("Arial, 14"));
