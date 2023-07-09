77872a83eaefb87be3653427714d6c878aa28ff1
cokorie <chijioke.okorie@gmail.com>
2023-07-09 12:30:30 -0500

    change text result button font to Courier

--- calculator/main.py
+++ calculator/main.py
@@ -26,7 +26,7 @@ def clear_field():
 root = tk.Tk();
 root.geometry("300x275");
 
-text_result = tk.Text(root, height=2, width=16, font=("Arial", 24));
+text_result = tk.Text(root, height=2, width=16, font=("Courier", 24));
 text_result.grid(columnspan=5);
 
 btn_1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1), width=5, font=("Courier, 14"));
