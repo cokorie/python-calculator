e21f2e6f81c1a5cc620891b8efe03d604f992a46
cokorie <chijioke.okorie@gmail.com>
2026-01-11 08:36:06 -0600

    update text result font to verdana

--- calculator/main.py
+++ calculator/main.py
@@ -32,7 +32,7 @@ root = tk.Tk();
 root.geometry("300x275");
 
 ## Code for displaying calculator input and results
-text_result = tk.Text(root, height=2, width=16, font=("Arial", 24));
+text_result = tk.Text(root, height=2, width=16, font=("Verdana", 24));
 text_result.grid(columnspan=5);
 
 ## Section for calculator buttons, 1 through 0
