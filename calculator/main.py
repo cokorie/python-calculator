35482c294774092cf199b49d8b807dec635fd5f9
cokorie <chijioke.okorie@gmail.com>
2026-05-06 09:34:54 -0500

    revert orig clear end message

--- calculator/main.py
+++ calculator/main.py
@@ -25,7 +25,7 @@ def eval_calculation():
 def clear_field():
     global calculation;
     calculation = "";
-    text_result.delete(1.0, "END");
+    text_result.delete(1.0, "end");
 
 ## Code for Tkinter GUI formation
 root = tk.Tk();
