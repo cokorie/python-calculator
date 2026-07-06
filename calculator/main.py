264e12574dd2deb6a1c42ba72fe4d0353e05e401
cokorie <chijioke.okorie@gmail.com>
2026-07-05 19:06:16 -0500

    add plus btn arial font

--- calculator/main.py
+++ calculator/main.py
@@ -67,7 +67,7 @@ btn_0 = tk.Button(root, text="0", command=lambda: add_to_calculation(0), width=5
 btn_0.grid(row=5, column=2);
 
 ## Section for math operator buttons (+, -, * and /)
-btn_plus = tk.Button(root, text="+", command=lambda: add_to_calculation("+"), width=5, font=("Arial Black, 14"));
+btn_plus = tk.Button(root, text="+", command=lambda: add_to_calculation("+"), width=5, font=("Arial, 14"));
 btn_plus.grid(row=2, column=4);
 
 btn_minus = tk.Button(root, text="-", command=lambda: add_to_calculation("-"), width=5, font=("Arial Black, 14"));
