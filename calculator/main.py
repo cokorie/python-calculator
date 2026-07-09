42daadb29a095774dd933b5f6ed1e2892245d5d7
cokorie <chijioke.okorie@gmail.com>
2026-07-08 19:17:27 -0500

    add close parens btn arial font

--- calculator/main.py
+++ calculator/main.py
@@ -83,7 +83,7 @@ btn_div.grid(row=5, column=4);
 btn_open_paren = tk.Button(root, text="(", command=lambda: add_to_calculation("("), width=5, font=("Arial, 14"));
 btn_open_paren.grid(row=5, column=1);
 
-btn_close_paren = tk.Button(root, text=")", command=lambda: add_to_calculation(")"), width=5, font=("Arial Black, 14"));
+btn_close_paren = tk.Button(root, text=")", command=lambda: add_to_calculation(")"), width=5, font=("Arial, 14"));
 btn_close_paren.grid(row=5, column=3);
 
 btn_clear = tk.Button(root, text="C", command=clear_field, width=11, font=("Arial Black, 14"));
